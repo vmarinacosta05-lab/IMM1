6cf95708d998c70642a8e630f3557ceacaa945eb
vmarinacosta05-lab <vmarinacosta05@gmail.com>
2026-02-26 13:56:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.write('Do you ever feel like a plastic bag '  
+         ' Drifting through the wind ' 
+         ' Wanting to start again? '  
+         'Do you ever feel, feel so paper thin ' 
+         ' Like a house of cards '
+         ' One blow from caving in?'
         
         )
            
